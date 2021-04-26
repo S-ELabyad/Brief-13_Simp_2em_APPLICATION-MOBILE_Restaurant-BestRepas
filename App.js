14b34739f7e9f06b7b8a57e1c48fa3ec093e2dd3
saloua <salouaelabyad@gmail.com>
2021-04-26 16:04:15 +0000

    add checkout

--- App.js
+++ App.js
@@ -1,126 +1,188 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Text, StyleSheet,SafeAreaView, View ,Image } from 'react-native';
-import SearchBar from './src/components/SearchBar';
-import ItemType from './src/screens/home/ItemType';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
-const type = [
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Image,
+} from 'react-native';
+import SearchBar from './src/components/SearchBar';
+import BottomBar from './src/screens/home/BottomBar';
+import ItemType from './src/screens/home/ItemType';
+import RestaurantItem from './src/screens/home/RestaurantItem';
+import ModalOrder from './src/screens/order/ModalOrder';
+
+const types = [
   {
-    type : 'Dinner',
+    type: 'Dinner',
     image: require('./src/assets/images/dinner.png'),
   },
   {
-    type : 'Lunch',
+    type: 'Lunch',
     image: require('./src/assets/images/lunch.png'),
   },
   {
-    type : 'Breakfast',
+    type: 'Breakfast',
     image: require('./src/assets/images/breakfast.png'),
   },
   {
-    type : 'Dessert',
+    type: 'Dessert',
     image: require('./src/assets/images/dessert.png'),
   },
 ];
 
+const listRestaurants = [
+  {
+    background: require('./src/assets/images/bread.png'),
+    miles: 10,
+    name: `Noah's Bagel`,
+    type: 'Lunch',
+    country: 'American',
+    currency: '$',
+    smile: 97,
+  },
+  {
+    background: require('./src/assets/images/pho.png'),
+    miles: 10,
+    name: `Pho Saigon`,
+    type: 'Lunch',
+    country: 'VietNam',
+    currency: 'VND',
+    smile: 97,
+  },
+];
+
 const App = () => {
+  const [isOrder, setIsOrder] = useState(false);
+  const onPressItem = () => setIsOrder(true);
+  const renderRestaurant = (item) => {
+    return <RestaurantItem data={item} onPress={onPressItem} />;
+  };
+  const [IdSelected , setIdSelected] = useState(1);
+  const onClose = () => setIsOrder(false);
+  const onSelected = (id) => {
+    // alert('id' + id);
+    setIdSelected(id);
+  };
+  const _ fectListFood = async() => {
+try {
+  const res = await
+} catch (error) {
+  
+}
+  };
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView style={styles.container} >
-       <View style={styles.wrap}>
-        <View style={styles.header}>
-
+      <SafeAreaView style={styles.container}>
+        <ScrollView style={styles.wrap} showsVerticalScrollIndicator={false}>
+          <View style={styles.header}>
             <View style={styles.headerTitle}>
-                <Text style={styles.heading}> What do you have a taste for ?</Text>
-                <Text style={styles.description}>1240 Restaurant Availabe</Text>
+              <Text style={styles.heading}>What do you have a taste for?</Text>
+              <Text style={styles.description}>1240 Restaurants availabe</Text>
             </View>
-            
+
             <View style={styles.headerCart}>
               <View style={styles.buttonCart}>
-                    <Image style={styles.img} source={require('./src/assets/images/icon.png')} /> 
-                    <Text style={styles.numCart}>2</Text>
+                <Image source={require('./src/assets/images/cart.png')} />
+                <Text style={styles.numCart}>2</Text>
               </View>
             </View>
-
-        </View>  
-
-          <SearchBar />
-
-          <View style={styles.listItemType}>
-            {type.map((item) => {
-              return <ItemType {...item} />;
-            })}
-            
-        
           </View>
-
-        
-          </View>  
-
+          <SearchBar />
+          <View style={styles.listItemType}>
+            {types.map((item, index) => {
+              return <ItemType 
+              onSelected={onSelected} 
+              selected={item.id == IdSelected} 
+              {...item} />;
+            })}
+          </View>
+          <View style={styles.listRestaurant}>
+            <Text style={styles.listRestaurantText}>
+              Lunch Restaurants Near You
+            </Text>
+            <View style={styles.listRes}>
+              {listRestaurants.map(renderRestaurant)}
+            </View>
+          </View>
+        </ScrollView>
+        <BottomBar />
       </SafeAreaView>
+      {isOrder && <ModalOrder onClose={onClose} isShow={isOrder} />}
     </>
   );
-}
+};
 
 const styles = StyleSheet.create({
+  listRestaurantText: {
+    fontFamily: 'CeraPro-Medium',
+    fontSize: 18,
+  },
   container: {
     flex: 1,
   },
   wrap: {
-    flex:1,
-    padding:14,
+    flex: 1,
+    padding: 14,
   },
-  headerTitle : {
-  width: '55%',
+  headerTitle: {
+    width: '55%',
   },
-  headerCart : {
-    marginLeft: 60,
-  },
-  heading : {
-    fontSize : 25 ,
-    // fontFamily: 'CeraPro-Bold',
-    fontWeight: 'bold',
+  headerCart: {},
+  heading: {
+    fontSize: 25,
+    fontFamily: 'CeraPro-Bold',
     lineHeight: 30,
-   
   },
-  description :{
-    color : 'gray',
+  description: {
+    fontFamily: 'CeraPro-Medium',
+    color: 'gray',
     marginTop: 10,
   },
-  buttonCart : {
-    backgroundColor : '#F7CB6B',
+  buttonCart: {
+    backgroundColor: '#F7CB6B',
     padding: 12,
-    flexDirection : 'row',
-    borderRadius : 15,
-    shadowOpacity :0.4,
-    shadowColor : '#F7CB6B',
-    shadowRadius :2,
-    marginTop :15,
-
-  },
-  img : {
-  width: 23 ,
-  height:30,
- 
-   
-  },
- 
-  header : {
     flexDirection: 'row',
-    padding : 16,
+    borderRadius: 15,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.4,
+    shadowColor: '#F7CB6B',
+    shadowRadius: 2,
+  },
+  header: {
+    flexDirection: 'row',
+    padding: 16,
     justifyContent: 'space-between',
   },
-  numCart : {
-    color : '#FFF',
+  numCart: {
+    fontFamily: 'CeraPro-Medium',
+    color: '#FFF',
     paddingLeft: 10,
   },
-  listItemType : {
-    flexDirection : 'row',
+  listItemType: {
+    flexDirection: 'row',
   },
- 
+  listRes: {
+    marginBottom: 15,
+  },
 
+  listRestaurant: {
+    marginTop: 10,
+  },
 });
 
-export default App 
- 
+export default App;
